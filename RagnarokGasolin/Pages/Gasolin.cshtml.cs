ead19199f75c1a6a5553f9242f702a3f294b36ac
lars_ <lars_@THEWHITEDEMON>
2025-03-17 14:12:08 +0100

    Add project files.

---
+++ RagnarokGasolin/Pages/Gasolin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarokGasolin.Pages
+{
+    public class GasolinModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
