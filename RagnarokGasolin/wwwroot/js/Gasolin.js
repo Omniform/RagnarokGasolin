24e8d64afb67272ecd8e8ac0fe1c7a34cd6133ae
Lasermorph <stefanashansen@hotmail.com>
2025-03-18 22:36:00 +0100

    small name changes

--- RagnarokGasolin/wwwroot/js/Gasolin.js
+++ RagnarokGasolin/wwwroot/js/Gasolin.js
@@ -1,25 +1,25 @@
 let list = document.getElementById("bandMembers");
-let data = [];
+let artistNames = [];
 let linkList = [];
 
-function SetData(name, url)
+function AddArtist(name, url)
 {
-    data.push(name);
+    artistNames.push(name);
     linkList.push(url);
 }
 
-SetData("Kim Larsen", "/Gasolin/KimLarsen");
-SetData("Franz Beckerlee", "/Gasolin/Franz Beckerlee");
-SetData("Wili Jønsson", "/Gasolin/Wili Jønsson");
-SetData("Søren Berlev", "/Gasolin/Søren Berlev");
-SetData("Bjørn Uglebjerg", "/Gasolin/Bjørn Uglebjerg");
-SetData("gamer", "https://youtube.com");
+AddArtist("Kim Larsen", "/Gasolin/KimLarsen");
+AddArtist("Franz Beckerlee", "/Gasolin/Franz Beckerlee");
+AddArtist("Wili Jønsson", "/Gasolin/Wili Jønsson");
+AddArtist("Søren Berlev", "/Gasolin/Søren Berlev");
+AddArtist("Bjørn Uglebjerg", "/Gasolin/Bjørn Uglebjerg");
+AddArtist("gamer", "https://youtube.com");
 
-for (i = 0; i < data.length; i++)
+for (i = 0; i < artistNames.length; i++)
 {
     let li = document.createElement("li");
     let a = document.createElement("a");
-    a.innerText = data[i];
+    a.innerText = artistNames[i];
     li.classList.add("nav-item");
     a.classList.add("nav-link");
     a.classList.add("text-light");
