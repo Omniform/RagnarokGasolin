0c3b3b11766a521dea6aaa973e2dfbbc8df25b92
Lars <lav005@edu.zealand.dk>
2025-03-18 22:34:16 +0100

    Forsøg på at få billederne i karusellen til at være mere samme størrelse. Virker okay, men ikke, hvis skærmen bliver for lille

--- RagnarokGasolin/wwwroot/js/site.js
+++ RagnarokGasolin/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    console.log($('#carouselwithIndicators').css('height')); // check the initial height of the carousel;
+
+    // now apply this height as a max-height on all the image items; css will handle the rest;
+    $('#carouselwithIndicators').find('.carousel-item img').css('max-height', $('#carouselwithIndicators').css('height'))
+});
