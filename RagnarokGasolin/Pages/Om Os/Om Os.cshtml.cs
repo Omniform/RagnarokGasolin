af524ace6a795d04fff83d24bab495680a4e6c5a
TokeDit <Tokedit11@gmail.com>
2025-03-18 13:29:33 +0100

    Ændringer i header og navbar med flere sider

---
+++ RagnarokGasolin/Pages/Om Os/Om Os.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarokGasolin.Pages.Om_Os
+{
+    public class Om_OsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RagnarokGasolin/Pages/Udstillinger/Udstillinger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarokGasolin.Pages.Udstillinger
+{
+    public class UdstillingerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
