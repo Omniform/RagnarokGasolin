2189dbff869cc1e71557a6418f59924dd0e82bb2
Lasermorph <stefanashansen@hotmail.com>
2025-03-19 13:40:47 +0100

    javascript som åbenbart er værdiløst

--- RagnarokGasolin/wwwroot/js/Gasolin.js
+++ RagnarokGasolin/wwwroot/js/Gasolin.js
@@ -1,18 +1,61 @@
-let list = document.getElementById("bandMembers");
-let drIV = document.getElementById("drInterview");
-let artistNames = [];
-let linkList = [];
+let m_cardParentDiv = document.getElementById("bandMembers");
+let m_drIV = document.getElementById("drInterview");
+let m_artistNames = [];
+let m_linkList = [];
 
-list.style.cssFloat = "left";
-drIV.style.cssFloat = "right";
+m_cardParentDiv.classList.add("container");
+m_cardParentDiv.style.cssFloat = "left";
 
-drIV.setAttribute("src", "https://www.youtube.com/embed/PoHwxGFGj3s?si=RPJayMzGS4NkDrCK");
-drIV.style.scale = "2";
+m_drIV.style.cssFloat = "right";
+
+m_drIV.setAttribute("src", "https://www.youtube.com/embed/PoHwxGFGj3s?si=RPJayMzGS4NkDrCK");
+m_drIV.style.scale = "2";
 
 function AddArtist(name, url)
 {
-    artistNames.push(name);
-    linkList.push(url);
+    m_artistNames.push(name);
+    m_linkList.push(url);
+}
+
+function CreateCard(title, text, buttonText, link, imgPath)
+{
+    let cardChildDiv = document.createElement("div");
+    let img = document.createElement("img");
+    let body = document.createElement("div");
+    let header = document.createElement("h5");
+    let p = document.createElement("p");
+    let a = document.createElement("a");
+
+    cardChildDiv.classList.add("card");
+    cardChildDiv.style.width = "18rem";
+    cardChildDiv.style.cssFloat = "left";
+
+    img.classList.add("card-img-top");
+    img.setAttribute("src", link);
+    img.setAttribute("alt", title + " image");
+    img.setAttribute("src", imgPath);
+
+    body.classList.add("card-body");
+
+    header.classList.add("card-title");
+    header.innerText = title;
+
+    p.classList.add("card-text");
+    p.innerText = text;
+
+    a.classList.add("btn");
+    a.classList.add("btn-primary");
+    a.setAttribute("href", link);
+    a.innerText = buttonText;
+
+    body.appendChild(header);
+    body.appendChild(p);
+    body.appendChild(a);
+
+    cardChildDiv.appendChild(img);
+    cardChildDiv.appendChild(body);
+
+    return cardChildDiv;
 }
 
 AddArtist("Kim Larsen", "/Gasolin/KimLarsen");
@@ -22,16 +65,18 @@ AddArtist("Søren Berlev", "/Gasolin/Søren Berlev");
 AddArtist("Bjørn Uglebjerg", "/Gasolin/Bjørn Uglebjerg");
 AddArtist("gamer", "https://youtube.com");
 
-for (i = 0; i < artistNames.length; i++)
+for (i = 0; i < m_artistNames.length; i++)
 {
-    let li = document.createElement("li");
-    let a = document.createElement("a");
-    a.innerText = artistNames[i];
-    li.classList.add("nav-item");
-    a.classList.add("nav-link");
-    a.classList.add("text-light");
+    // let cardChildDiv = document.createElement("div");
+    // let a = document.createElement("a");
+    // let cardImage = document.createElement("img");
 
-    a.setAttribute("href", linkList[i]);
-    li.appendChild(a);
-    list.appendChild(li);
+    // a.innerText = m_artistNames[i];
+
+    // cardChildDiv.classList.add("card");
+    // cardChildDiv.style.width = "18rem";
+
+    // a.setAttribute("href", m_linkList[i]);
+
+    m_cardParentDiv.appendChild(CreateCard(m_artistNames[i], "this is it", "press me", m_linkList[i], "..."));
 }
