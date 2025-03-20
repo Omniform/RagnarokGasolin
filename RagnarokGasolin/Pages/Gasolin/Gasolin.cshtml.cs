91784bf062011a74329926e216b71a3a55740a2f
Lasermorph <stefanashansen@hotmail.com>
2025-03-20 23:29:21 +0100

    Creating the cards instance in a forloop

--- RagnarokGasolin/Pages/Gasolin/Gasolin.cshtml.cs
+++ RagnarokGasolin/Pages/Gasolin/Gasolin.cshtml.cs
@@ -7,12 +7,113 @@ namespace RagnarokGasolin.Pages.Gasolin
     {
         private readonly ILogger<GasolinModel> _logger;
 
+        public MtkArtist m_artists = new();
+
         public GasolinModel(ILogger<GasolinModel> logger)
         {
             _logger = logger;
         }
+
+        // public List<string> GetArtists()
+        // {
+        //     return m_artists.GetArtists();
+        // }
+
+        // public List<string> OnGetGetLink()
+        // {
+        //     return m_artists.GetLinks();
+        // }
+
+        public string OnGetGetArtist(int position)
+        {
+            return m_artists.GetArtist(position);
+        }
+
+        public string OnGetGetLink(int position)
+        {
+            return m_artists.GetLink(position);
+        }
+
         public void OnGet()
         {
         }
+
+        public void OnGetAddArtists()
+        {
+            m_artists.Add("Kim Larsen", "/Gasolin/KimLarsen");
+            m_artists.Add("Franz Beckerlee", "/Gasolin/Franz Beckerlee");
+            m_artists.Add("Wili Jønsson", "/Gasolin/Wili Jønsson");
+            m_artists.Add("Søren Berlev", "/Gasolin/Søren Berlev");
+            m_artists.Add("Bjørn Uglebjerg", "/Gasolin/Bjørn Uglebjerg");
+        }
     }
+
+    public class MtkArtist
+    {
+        private List<string> m_artistNames = new();
+        private List<string> m_links = new();
+
+        public MtkArtist(){}
+
+        public string GetArtist(int artistPosition)
+        {
+            return m_artistNames[artistPosition];
+        }
+
+        public string GetLink(int linkPosition)
+        {
+            return m_links[linkPosition];
+        }
+
+        public List<string> GetArtists()
+        {
+            return m_artistNames;
+        }
+
+        public List<string> GetLinks()
+        {
+            return m_links;
+        }
+
+        public void Add(string artist, string link)
+        {
+            m_artistNames.Add(artist);
+            m_links.Add(link);
+        }
+    }
+
+
+    // public class MtkArtist<T, T2>
+    // {
+    //     private List<T> m_artistNames = new();
+    //     private List<T2> m_links = new();
+
+    //     public MtkArtist(){}
+
+    //     public T GetArtist(int artistPosition)
+    //     {
+    //         return m_artistNames[artistPosition];
+    //     }
+
+    //     public T2 GetLink(int linkPosition)
+    //     {
+    //         return m_links[linkPosition];
+    //     }
+
+    //     public List<T> GetArtists()
+    //     {
+    //         return m_artistNames;
+    //     }
+
+    //     public List<T2> GetLinks()
+    //     {
+    //         return m_links;
+    //     }
+
+    //     public void Add(T artist, T2 link)
+    //     {
+    //         m_artistNames.Add(artist);
+    //         m_links.Add(link);
+    //     }
+    // }
 }
