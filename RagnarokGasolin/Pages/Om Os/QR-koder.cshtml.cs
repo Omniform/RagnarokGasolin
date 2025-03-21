a8fe723715a37f19acfa4826be6cafcc0ad8137c
TokeDit <Tokedit11@gmail.com>
2025-03-21 11:00:43 +0100

    added QR codes, and new tab in navbar containing the QR kodes

---
+++ RagnarokGasolin/Pages/Om Os/QR-koder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarokGasolin.Pages.Om_Os
+{
+    public class QR_koderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
